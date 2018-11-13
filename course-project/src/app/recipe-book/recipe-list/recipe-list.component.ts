71389ea5e201530e529586587b0ce0fded82636e
Valentin Omnes <valentin.omnes@gmail.com>
2018-11-13 19:11:03 +0100

    feat: Add recipe-list visual [course project]

--- course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipeBook } from '../recipe-book.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: RecipeBook[] = [
+    new RecipeBook(
+      'A test',
+      'This is a test',
+      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
+    )
+  ];
 
   constructor() { }
 
