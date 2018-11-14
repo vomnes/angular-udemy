cc7d85bfd4da797b0d0523993cfad9625f1d2bc3
Valentin OMNES <vomnes@student.42.fr>
2018-11-14 20:02:04 +0100

    feat: Add ingredients through references [course-project]

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +15,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() addIngredient = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addElem() {
+    const ingName = this.nameInput.nativeElement.value;
+    const ingAmount = this.amountInput.nativeElement.value;
+    if (ingName !== '' && ingAmount !== 0) {
+      this.addIngredient.emit(new Ingredient(ingName, ingAmount));
+    }
+  }
 }
