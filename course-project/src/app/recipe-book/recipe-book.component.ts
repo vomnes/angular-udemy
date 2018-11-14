ee61774efe8e27d9854c23983314e6aa06fb06c3
Valentin OMNES <vomnes@student.42.fr>
2018-11-14 19:20:01 +0100

    feat: Add view recipe details [course-project]

--- course-project/src/app/recipe-book/recipe-book.component.ts
+++ course-project/src/app/recipe-book/recipe-book.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipeBook } from './recipe-book.model';
+
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
   styleUrls: ['./recipe-book.component.css']
 })
 export class RecipeBookComponent implements OnInit {
+  selectedRecipe: RecipeBook = null;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  recipeSelected(data: RecipeBook) {
+    this.selectedRecipe = data;
+  }
 }
--- course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { RecipeBook } from '../recipe-book.model';
 
@@ -8,10 +8,17 @@ import { RecipeBook } from '../recipe-book.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<RecipeBook>();
+
   recipes: RecipeBook[] = [
     new RecipeBook(
       'A test',
       'This is a test',
+      'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
+    ),
+    new RecipeBook(
+      'B test',
+      'This is B test',
       'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
     )
   ];
@@ -21,4 +28,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  selectRecipe(data: RecipeBook) {
+    this.recipeSelected.emit(data);
+  }
 }
