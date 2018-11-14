bc5f3cc138288353574c10d50311a3893aaf4ae1
Valentin OMNES <vomnes@student.42.fr>
2018-11-14 18:24:50 +0100

    feat: Add navigation [course-project]

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentTab = 'shopping';
+
+  updateTab(tabName: string) {
+    this.currentTab = tabName;
+  }
 }
--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() changeTab = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateTab(newName) {
+    this.changeTab.emit(newName);
   }
 
 }
