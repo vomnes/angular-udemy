3ce215dd63c522ce5a00639b58268d748772e364
Valentin Omnes <valentin.omnes@gmail.com>
2018-11-15 12:00:12 +0100

    feat: Add directives-start

---
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appBasicHighlight]',
+})
+
+export class BasicHighlightDirective implements OnInit {
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngOnInit() {
+    this.elementRef.nativeElement.style.backgroundColor = 'green';
+  }
+}
