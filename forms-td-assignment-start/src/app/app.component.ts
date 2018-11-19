4050e8c854ddc0da8e2e2467ad2dd53e183781a6
Valentin OMNES <vomnes@student.42.fr>
2018-11-19 19:50:46 +0100

    feat: Finish my angular-udemy/forms-td-assignment-start

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  subscriptionsTypes = [
+    'Basic',
+    'Advanced',
+    'Pro'
+  ];
+  selectedOption = 'Advanced';
+  submitted = false;
+  user = {
+    email: '',
+    subscriptionType: '',
+    password: '',
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value.email);
+    console.log(this.selectedOption);
+    console.log(this.signupForm.value.password);
+
+    this.submitted = true;
+    this.user.email = this.signupForm.value.email;
+    this.user.subscriptionType = this.selectedOption;
+    this.user.password = this.signupForm.value.password;
+    this.signupForm.reset();
+  }
 
 }
