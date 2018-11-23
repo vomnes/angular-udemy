212125b478bef82bfa0de24635235df40c1b8e80
Valentin Omnès <valentin.omnes@gmail.com>
2018-11-23 15:02:00 +0100

    feat: Add PreloadAllModules [course-project]

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 // Shopping List
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -23,7 +23,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
